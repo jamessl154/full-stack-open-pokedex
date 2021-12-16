76f4fa8b288bd1595002b49c2d8c79fa23f6a4d3
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-12-16 15:35:50 +0000

    fixed comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+// https://expressjs.com/en/starter/static-files.html
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
