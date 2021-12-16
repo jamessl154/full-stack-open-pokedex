f5ff69ce5094f755b413cb9c6b09e764ab5419bb
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-12-16 15:59:36 +0000

    added e2e test for navigating to a single Pokemons page

--- cypress/integration/pokedex-e2e-test.js
+++ cypress/integration/pokedex-e2e-test.js
@@ -1,7 +1,16 @@
 describe('Pokedex', function() {
-  it('front page can be opened', function() {
+  beforeEach(() => {
     cy.visit('http://localhost:5000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('from the front page we can navigate to a single Pokemons page', () => { 
+    cy.contains('blastoise').click()
+    cy.contains('torrent')
+    cy.contains('rain dish')
   })
 })
