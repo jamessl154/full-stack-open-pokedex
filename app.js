88a11d459a4af8e5a858a5b8f78d41878d570a5a
James Sue-Ling <james_sueling@hotmail.co.uk>
2021-12-16 15:27:45 +0000

    need build before cypress tests current app requires a build step to run the cypress tests as we are serving static files from 'dist' on http://localhost:5000 that is gitignored in development

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/', (request, response) => {
-  response.send('Hello World!')
-})
-
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
